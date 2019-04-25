b555144a7df41a8212682e1e7596140a97381ee2
ashleychari1234 <ashley.c.francisco.21@dartmouth.edu>
2019-04-25 00:14:29 -0400

    routing added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-import $ from 'jquery';
 import './style.scss';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { Switch } from 'react-router';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 
-let num = 0;
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
 
-function counter() {
-  num += 1;
-  $('#main').html(`You've been on this page for ${num} seconds.`);
-}
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about" exact>About</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
 
-setInterval(() => { counter(); }, 1000);
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </div>
+    </Router>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('main'));
--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,12 @@ const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { 
 
 module.exports = {
   mode: env,
+  output: { publicPath: '/' },
   entry: ['babel-polyfill', './src'],
   devtool: 'source-map',
   devServer: {
     hot: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
@@ -67,6 +69,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: './index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './200.html',
     }),
   ],
 };

8eba3696650de4e05770da538059fe4e32acbcf9
Christopher Cheng <forever.21@dartmouth.edu>
2021-02-21 17:08:11 -0800

    now renders album video thumbnails

--- src/actions/albumActions.js
+++ src/actions/albumActions.js
@@ -45,3 +45,35 @@ export const addSharedAlbum = (userID, albumName, sharedWith) => {
     });
   };
 };
+
+export const getAlbums = (userID) => {
+  return (dispatch) => {
+    return new Promise((resolve, reject) => {
+      axios.get(`${API}/album/getAlbums/${userID}`)
+      .then((res) => {
+        dispatch({ type: ActionTypes.GET_ALBUMS, payload: res.data });
+        resolve();
+      }).catch((e) => {
+        console.log(`Error fetching albums: ${e}`);
+        reject(e);
+      });
+    });
+
+  };
+};
+
+export const getSharedAlbums = (userID) => {
+  return (dispatch) => {
+    return new Promise((resolve, reject) => {
+      axios.get(`${API}/album/getSharedAlbums/${userID}`)
+      .then((res) => {
+        dispatch({ type: ActionTypes.GET_SHARED_ALBUMS, payload: res.data });
+        resolve();
+      }).catch((e) => {
+        console.log(`Error fetching shared albums: ${e}`);
+        reject(e);
+      });
+    });
+
+  };
+};
